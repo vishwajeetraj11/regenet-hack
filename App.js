ad5e3462faf0e6475de89534d3907d09c9026934
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2022-12-09 21:16:04 +0530

    lint

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
-
-import { Navigation } from './components/Navigation';
-import {Fragment} from 'react'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { Navigation } from "./components/Navigation";
+import { Fragment } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 1 } },
 });
 
 export default function App() {
-  
   return (
     <Fragment>
-    <QueryClientProvider client={queryClient}>
-    <Navigation />
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <Navigation />
+      </QueryClientProvider>
     </Fragment>
   );
 }
